01c49d3e944ee716662b44ab5b20fcc88e13006a
Guilherme Alcantara <tnt.gamer00125@gmail.com>
2024-05-21 12:23:40 -0300

    aa

--- temp/script.js
+++ temp/script.js
@@ -1,5 +1,13 @@
-const corpo = document.getElementsByTagName('body')
+const corpo = document.getElementsByTagName('body');
 
 function fade() {
-    corpo[0].style.opacity = 0;
+	corpo[0].style.opacity = 0;
+	corpo[0].style.display = 'none';
+	// setTimeout(() => {
+	// 	window.location.replace('ajaj.html');
+	// }, 1000);
 }
+
+window.onload = () => {
+	corpo[0].style.opacity = 1;
+};
