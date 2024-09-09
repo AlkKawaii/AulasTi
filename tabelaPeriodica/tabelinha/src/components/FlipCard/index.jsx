525b6dc02657df3c34054ecb5e1365bf753970bf
Guilherme Alcantara <tnt.gamer00125@gmail.com>
2024-09-09 08:29:31 -0300

    flipcard indo pra algum lugar

--- tabelaPeriodica/tabelinha/src/components/FlipCard/index.jsx
+++ tabelaPeriodica/tabelinha/src/components/FlipCard/index.jsx
@@ -1,3 +1,4 @@
+import Element from '../Element';
 import styles from './FlipCard.module.css';
 import '@google/model-viewer';
 export default function FlipCard({ info }) {
@@ -5,26 +6,46 @@ export default function FlipCard({ info }) {
   let frontInfo;
   let backInfo;
 
+  if (info) {
+    frontInfo = (
+      <Element
+        atomicNum={info.number}
+        elementName={info.name}
+        group={info.category}
+        sym={info.symbol}
+      />
+    );
+    backInfo = (
+      <>
+        <model-viewer
+          src={info.bohr_model_3d}
+          alt='Modelo atômico 3D do elemento Alumínio'
+          autoplay=''
+          camera-controls=''
+          camera-orbit='-0.6751deg 70.99deg 1.4m'
+          exposure='0.3'
+          field-of-view='30deg'
+          max-camera-orbit='auto 180deg 1.4m'
+          min-camera-orbit='auto 0deg auto'
+          style={{
+            width: '60%',
+            height: '60%',
+            margin: 'auto',
+          }}></model-viewer>
+      </>
+    );
+    console.log(info.bohr_model_3d);
+  } else {
+    frontInfo = null;
+    backInfo = null;
+  }
+
   return (
     <div className={styles.flipCard}>
       <section className={styles.flipCardInner}>
-        <div className={styles.flipCardFront}></div>
+        <div className={styles.flipCardFront}>{frontInfo || nullInfo}</div>
         <article className={styles.flipCardBack}>
-          <model-viewer
-            src='https://storage.googleapis.com/search-ar-edu/periodic-table/element_013_aluminum/element_013_aluminum.glb'
-            alt='Modelo atômico 3D do elemento Alumínio'
-            autoplay=''
-            camera-controls=''
-            camera-orbit='-0.6751deg 70.99deg 1.4m'
-            exposure='0.3'
-            field-of-view='30deg'
-            max-camera-orbit='auto 180deg 1.4m'
-            min-camera-orbit='auto 0deg auto'
-            style={{
-              width: '60%',
-              height: '60%',
-              margin: 'auto',
-            }}></model-viewer>
+          {backInfo || nullInfo}
         </article>
       </section>
     </div>
