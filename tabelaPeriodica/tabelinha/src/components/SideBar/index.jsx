3740af1c208cfb7f81aac06eb32ac59c9938be67
Guilherme Alcantara <tnt.gamer00125@gmail.com>
2024-09-12 22:05:05 -0300

    Update index.jsx

--- tabelaPeriodica/tabelinha/src/components/SideBar/index.jsx
+++ tabelaPeriodica/tabelinha/src/components/SideBar/index.jsx
@@ -1,34 +1,46 @@
-import { Link } from "react-router-dom";
-import styles from "./SideBar.module.css";
-import { useState } from "react";
+import { Link } from 'react-router-dom';
+import styles from './SideBar.module.css';
+import { useEffect, useRef, useState } from 'react';
 
 export default function SideBar() {
   const [isOpen, setIsOpen] = useState(false);
+  const sideBarRef = useRef(null);
+  useEffect(() => {
+    const handleOutsideClick = (e) => {
+      if (sideBarRef.current && !sideBarRef.current.contains(e.target)) {
+        setIsOpen(false);
+      }
+    };
+    document.addEventListener('mousedown', handleOutsideClick);
 
+    return () => {
+      document.removeEventListener('mousedown', handleOutsideClick);
+    };
+  }, [isOpen]);
   function toggleSideBar() {
-    setIsOpen(!isOpen);
+    setIsOpen((prev) => !prev);
   }
 
   return (
     <>
       <span
-        class={`material-symbols-outlined ${styles.sideMenuButton}`}
-        onClick={toggleSideBar}
-      >
+        className={`material-symbols-outlined ${styles.sideMenuButton}`}
+        onClick={toggleSideBar}>
         menu
       </span>
-      <nav className={`${styles.sideMenu} ${isOpen ? styles.open : ""}`}>
+      <nav
+        className={`${styles.sideMenu} ${isOpen ? styles.open : ''}`}
+        ref={sideBarRef}>
         <h2>Links Úteis</h2>
         <span
           className={`material-symbols-outlined ${styles.sideMenuClose} ${styles.sideMenuButton}`}
-          onClick={toggleSideBar}
-        >
+          onClick={toggleSideBar}>
           close
         </span>
-        <Link to="/" className={styles.link}>
+        <Link to='/' className={styles.link}>
           <span>Chemical</span>
         </Link>
-        <Link to="/table" className={styles.link}>
+        <Link to='/table' className={styles.link}>
           <p> Tabela Periódica</p>
         </Link>
       </nav>
